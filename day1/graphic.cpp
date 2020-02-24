505d9203ab67918437ab81b460c19f900515e98a
wolwolton <yusakuwithniko@gmail.com>
2020-02-24 15:24:31 +0900

    init screen

--- day1/graphic.cpp
+++ day1/graphic.cpp
@@ -1,2 +1,32 @@
 #include "gop.hpp"
 #include "graphic.hpp"
+
+void boxfill(GOPMODE* mode, int x1, int y1, int x2, int y2, const COLOR& c){
+    for(int ix = x1; ix<x2; ix++){
+        for(int iy=y1; iy<y2; iy++){
+            BLTPIXEL* p = (BLTPIXEL*)(mode->FrameBufferBase + (ix + mode->Info->Hr * iy)*sizeof(BLTPIXEL));
+            p->Blue  =c.Blue;
+            p->Green =c.Green;
+            p->Red   =c.Red;
+        }
+    }
+    return;
+}
+
+void init_screen(GOPMODE* mode){
+    int hr = mode->Info->Hr;
+    int vr = mode->Info->Vr;
+    boxfill(mode, 0, 0, hr, vr, main_color);
+
+    boxfill(mode, 0, vr-36, hr, vr, white);
+    boxfill(mode, 0, vr-35, hr, vr, pannel);
+
+    boxfill(mode, 3, vr-31, 51, vr-3, black);
+    boxfill(mode, 3, vr-31, 50, vr-4, white) ;
+    boxfill(mode, 4, vr-30, 50, vr-4, pannel);
+    
+    boxfill(mode, hr-50, vr-32, hr-2, vr-3, white);
+    boxfill(mode, hr-50, vr-32, hr-3, vr-4, pannel_shadow);
+    boxfill(mode, hr-49, vr-31, hr-3, vr-4, pannel);
+    return;
+}
--- day1/graphic.hpp
+++ day1/graphic.hpp
@@ -1,2 +1,16 @@
 #pragma once
 #include "gop.hpp"
+
+typedef BLTPIXEL    COLOR;
+
+const COLOR green = {0,255,0};
+const COLOR blue  = {255,0,0};
+const COLOR red   = {0,0,255};
+const COLOR white = {0xff,0xff,0xff};
+const COLOR black = {0x00,0x00,0x00};
+const COLOR main_color = {64,96,48};
+const COLOR pannel = {0xc6,0xc6,0xc6};
+const COLOR pannel_shadow = {0x84,0x84,0x84};
+
+void boxfill(GOPMODE* mode,const int x1,const int y1,const int x2,const int y2,const COLOR& c);
+void init_screen(GOPMODE* mode);
